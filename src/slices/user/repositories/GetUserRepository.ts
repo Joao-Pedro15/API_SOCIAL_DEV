9ac3027ceaf7101b83f8a94d3f7b43b96e9bc6e4
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-04 17:47:03 -0300

    feat: create a get repository user abstract

---
+++ src/slices/user/repositories/GetUserRepository.ts
@@ -0,0 +1,8 @@
+import { UserEntity, UserData } from "@/slices/user/entities/UserEntity"
+
+export interface GetUserRepository{
+    getAllUsers () : Promise<UserData[] | [] | null>
+    getUserByEmail (email: string) : Promise<UserData | null>
+    getUserById (id: number) : Promise<UserData | null>
+}
+
