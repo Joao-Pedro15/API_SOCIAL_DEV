414e3ad5016ecf2dde973f455a5ec56741d13d1c
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-13 18:17:08 -0300

    chore: change return promise in getUser from [] to null

--- src/slices/user/repositories/GetUserRepository.ts
+++ src/slices/user/repositories/GetUserRepository.ts
@@ -1,7 +1,7 @@
 import { UserEntity, UserData } from "@/slices/user/entities/UserEntity"
 
 export interface GetUserRepository{
-    getAllUsers () : Promise<UserData[] | [] | null>
+    getAllUsers () : Promise<UserData[] | null>
     getUserByEmail (email: string) : Promise<UserData | null>
     getUserById (id: number) : Promise<UserData | null>
 }
