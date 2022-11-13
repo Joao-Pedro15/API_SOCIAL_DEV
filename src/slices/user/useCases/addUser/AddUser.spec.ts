0d135ef72b54054d37a9c70a97a1370c242321ac
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-13 18:29:16 -0300

    chore: delete trash lines

--- src/slices/user/useCases/addUser/AddUser.spec.ts
+++ src/slices/user/useCases/addUser/AddUser.spec.ts
@@ -1,21 +1,16 @@
-import exp from 'constants'
 import { MockProxy, mock } from 'jest-mock-extended'
 import { UserEntityFake } from '../../entities/UserEntity.spec'
 import { AddUserRepository, GetUserRepository } from '../../repositories'
 import { AddUserUseCase } from './AddUser'
 
 describe('addUser', () => {
-    let testInstance : any
     let addUserRepository : MockProxy<AddUserRepository>
     let getUserRepository : MockProxy<GetUserRepository>
-    let addUserUseCase : MockProxy<AddUserUseCase>
     beforeEach(async () => {
         addUserRepository = mock()
         addUserRepository.addUser.mockResolvedValue({...UserEntityFake, email: 'samuel@gmail.com'})
         getUserRepository = mock()
         getUserRepository.getUserByEmail.mockResolvedValue(null)
-        // addUserUseCase = mock()
-        // testInstance = await new AddUserUseCase(addUserRepository, getUserRepository).execute({...UserEntityFake, email: '3'})
     })
 
     it('should create and return error message email invalid! ', async () => {
