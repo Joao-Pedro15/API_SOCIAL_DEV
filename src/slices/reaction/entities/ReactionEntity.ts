7e77a7052a5ff72d5b8e2615bc9bd922247a3963
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-17 21:20:02 -0300

    feat: create a Reaction Entity

---
+++ src/slices/reaction/entities/ReactionEntity.ts
@@ -0,0 +1,27 @@
+interface ReactionData {
+  userId: number
+  postId?: number | null
+  commentId?: number | null 
+  reactionType: string
+}
+
+export class ReactionEntity {
+  public userId: number
+  public postId?: number | null
+  public commentId?: number | null
+  public reactionType: string
+  constructor(data: ReactionData) {
+    Object.assign(this, data)
+  }
+  
+  static create(data: ReactionData) {
+    if(!this.validate(data)) throw new Error('Invalid data object')
+  }
+
+  static validate(data: ReactionData) : boolean {
+    if(!data.commentId && !data.postId) return false
+    return true
+  }
+
+
+}
