b128b445ede98fb74652bc16488d41f142a08e0e
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-19 19:49:40 -0300

    chore: implements validation in addReactionUseCase

--- src/slices/reaction/useCases/addReaction/AddReactionUseCase.ts
+++ src/slices/reaction/useCases/addReaction/AddReactionUseCase.ts
@@ -1,11 +1,17 @@
 import { AddReactionRepository } from '@/slices/reaction/repository/AddReactionRepository'
+import { GetPostRepository } from '@/slices/post/repositories/GetPostRepository'
+// import {  } from'@/slices/comment/repositories'
 import { ReactionData, ReactionEntity } from '@/slices/reaction/entities/ReactionEntity'
 
 export class AddReactionUseCase {
   constructor(
-    private addRepository: AddReactionRepository
+    private addRepository: AddReactionRepository,
+    private getPostRepository: GetPostRepository
   ){}
   async execute(data: ReactionData) {
+    const post = await this.getPostRepository.getPostById(data.postId)
+    if(!post) throw new Error('cannot find post or comment')
+
     try {
       const reactionEntity = ReactionEntity.create(data)
       const reaction = await this.addRepository.addReaction(reactionEntity)
