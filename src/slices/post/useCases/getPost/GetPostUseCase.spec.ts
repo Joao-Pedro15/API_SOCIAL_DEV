096a55d378e78eec7e9ff9cf65d5c990fa3b3f03
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-04 15:12:51 -0300

    test: testing return null get all posts useCase

--- src/slices/post/useCases/getPost/GetPostUseCase.spec.ts
+++ src/slices/post/useCases/getPost/GetPostUseCase.spec.ts
@@ -9,4 +9,8 @@ describe('testing useCase getPostUseCase', () => {
     repository = mock()
     repository.getAllPosts.mockResolvedValue([])
   })
+  it('get all posts and returnn null', async() => {
+    let posts = await new GetPostUseCase(repository).execute('')
+    expect(posts).toBe(null)
+  })
 })
