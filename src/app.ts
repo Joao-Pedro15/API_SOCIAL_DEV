219dc86f1108760ee7a1b1473b1b26ec091b31f9
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-12 21:16:40 -0300

    feat: use /users api rest endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express'
+import { users } from './routes'
 
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+app.use('/users', users.default)
 
 app.get('/', async (request: Request, response: Response) => {
     return response.status(200).send('hello word')
