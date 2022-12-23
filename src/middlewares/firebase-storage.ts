07124d614e5950da903acb52373ca58d66628245
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-23 19:45:29 -0300

    feat: create a middleware by firebase storage

---
+++ src/middlewares/firebase-storage.ts
@@ -0,0 +1,35 @@
+import * as admin from "firebase-admin";
+
+import serviceAccount from '@/config/firebase-key.json'
+import { NextFunction, Request, Response } from "express";
+const service = JSON.stringify(serviceAccount)
+
+const BUCKET = "gs://social-dev-70b45.appspot.com"
+
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(service)),
+  storageBucket: BUCKET
+});
+
+const bucket = admin.storage().bucket()
+
+export const uploadImage = (request: Request, response: Response, next: NextFunction) => {
+  if(!request['file']) return next()
+  const image = request['file']
+  const nameFile = 'teste.jpg'
+  const file = bucket.file(nameFile)
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: image.mimetype
+    }    
+  })
+
+  stream.on('error', (error) => console.error(error))
+  stream.on('finish', async () => { 
+    await file.makePublic()
+    request['file'].firebaseUrl = `https://storage.googleapis.com/${BUCKET}/${nameFile}`
+    next()
+  })
+
+  stream.end(image.buffer)
+} 
