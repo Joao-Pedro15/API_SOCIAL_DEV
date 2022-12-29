e89c41222bf8ebd7cce41db2e4ee31f15f724e75
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-29 00:09:49 -0300

    feat: implementation repositories by user prisma instance

--- src/application/user/addUser/AddUserFactory.ts
+++ src/application/user/addUser/AddUserFactory.ts
@@ -1,16 +1,18 @@
 import { UserData } from '@/slices/user/entities/UserEntity'
-import { AddUserRepository } from '@/slices/user/repositories'
 import { AddUserUseCase } from '@/slices/user/useCases/addUser/AddUserUseCase'
+import { AddUserRepository, GetUserRepository } from '@/slices/user/repositories'
+import { User } from '@/application/user/prismaUser'
 
 class AddUserFactory {
   async build(data: UserData) {
     try {
-      const repo1:any = {}
-      const repo2:any = {}
-      const addUser = await new AddUserUseCase(repo1, repo2).execute(data)
+      const prismaUser = new User()
+      const addUserRepository = new AddUserRepository(prismaUser)
+      const getUserRepository = new GetUserRepository(prismaUser)
+      const addUser = await new AddUserUseCase(addUserRepository, getUserRepository).execute(data)
       return addUser
     } catch (error) {
-      return error
+      throw new Error(error.message)
     }
   }
 }
