e3b119db668ff6825eca0f97948e1f6c1aaba187
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-29 00:07:50 -0300

    feat: create a class from getUser respository

--- src/slices/user/repositories/GetUserRepository.ts
+++ src/slices/user/repositories/GetUserRepository.ts
@@ -1,8 +1,18 @@
 import { UserEntity, UserData } from "@/slices/user/entities/UserEntity"
 
-export interface GetUserRepository{
-    getAllUsers () : Promise<UserData[] | null>
-    getUserByEmail (email: string) : Promise<UserData | null>
-    getUserById (id: number) : Promise<UserData | null>
+export interface IGetUserRepository{
+    // getAll () : Promise<UserData[] | null>
+    getByEmail (email: string) : Promise<Partial<UserData> | null>
+    // getById (id: number) : Promise<UserData | null>
 }
 
+
+export class GetUserRepository {
+    constructor(
+        private getUserRepository: IGetUserRepository
+    ){}
+
+    async getByEmail(email: string) {
+        return await this.getUserRepository.getByEmail(email)
+    }
+}
