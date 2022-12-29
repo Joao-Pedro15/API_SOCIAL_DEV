677af1127126b0b734527c7007b26a6310a84b07
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-29 16:15:11 -0300

    feat: create class by addPostRepository

--- src/slices/post/repositories/AddPostRepository.ts
+++ src/slices/post/repositories/AddPostRepository.ts
@@ -1,5 +1,15 @@
 import { PostData } from '../entities/PostEntity'
 
-export interface AddPostRepository {
-  addPostRepository(data: PostData): Promise<void>
+export interface IAddPostRepository {
+  add(data: PostData): Promise<void>
+}
+
+export class AddPostRepository {
+  constructor(
+    private repository: IAddPostRepository
+  ) {}
+
+  async add(data: PostData) {
+    return await this.repository.add(data)
+  }
 }
