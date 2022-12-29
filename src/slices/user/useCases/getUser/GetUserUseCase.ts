a9386a53c1956e9009fe4541611b54df971d835f
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-29 16:16:26 -0300

    chore: comment fn not using and solved bug

--- src/slices/user/useCases/getUser/GetUserUseCase.ts
+++ src/slices/user/useCases/getUser/GetUserUseCase.ts
@@ -7,32 +7,32 @@ export class GetUserUseCase {
         private getUserRepository : GetUserRepository
     ){}
 
-    async getAllUsers() : Promise<UserData[] | null> {
-        try {
-            const users: UserData[] = await this.getUserRepository.getAllUsers()
-            return users
-        } catch (error) {
-            return error
-        }
-    }
+    // async getAllUsers() : Promise<UserData[] | null> {
+    //     try {
+    //         const users: UserData[] = await this.getUserRepository.getAllUsers()
+    //         return users
+    //     } catch (error) {
+    //         return error
+    //     }
+    // }
     
-    async getUserByEmail(email: string) : Promise<UserData | null> {
+    async getByEmail(email: string) : Promise<Partial<UserData> | null> {
         try {
             if(!email || typeof email !== 'string') throw new Error('Invalid email!')
-            const user = await this.getUserRepository.getUserByEmail(email)
+            const user = await this.getUserRepository.getByEmail(email)
             return user    
         } catch (error) {
             return error
         }
     }
 
-    async getUserById(id: number) : Promise<UserData | null> {
-        try {
-            if(!id) throw new Error('Invalid email!')
-            const user = await this.getUserRepository.getUserById(id)
-            return user    
-        } catch (error) {
-            return error
-        }
-    }
+    // async getUserById(id: number) : Promise<UserData | null> {
+    //     try {
+    //         if(!id) throw new Error('Invalid email!')
+    //         const user = await this.getUserRepository.getUserById(id)
+    //         return user    
+    //     } catch (error) {
+    //         return error
+    //     }
+    // }
 }
