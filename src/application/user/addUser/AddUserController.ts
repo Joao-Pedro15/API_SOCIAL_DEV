a7fc0c88bb5ee466a2430e391c06f0c4f5de387e
João Pedro <jjoao.monteiro15@gmail.com>
2022-12-29 00:10:13 -0300

    chore: change response route addUser

--- src/application/user/addUser/AddUserController.ts
+++ src/application/user/addUser/AddUserController.ts
@@ -4,9 +4,10 @@ import { Request, Response } from 'express'
 
 class AddUserController {
   async handle(request: Request, response: Response) {
-    const user : UserData = request.body
+    const { admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo } : UserData = request.body
     try {
-      const newUser = await factory.build(user)
+      const newUser = await factory.build({admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo})
+      
       return response.status(201).json({ message: 'successfully create user!', data: newUser })
     } catch (error:any) {
       return response.status(500).json({message: error.message})
