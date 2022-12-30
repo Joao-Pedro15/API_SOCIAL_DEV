053ac1edbdf118867b8ef69bcd4e85d214fbe7f3
João Pedro <joao.monteiro@veredaeducacao.com>
2022-12-30 20:44:52 -0300

    chore: create a testing route server with a express

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,14 @@ app.get('/', async (request: Request, response: Response) => {
     return response.status(200).send('hello word')
 })
 
+app.get('/testando', async (request: Request, response: Response) => {
+    return response.status(200).json([
+        { name: "Joao", age: 19 },
+        { name: "Pedro", age: 20 },
+        { name: "Correa", age: 21 },
+        { name: "Monteiro", age: 22 },
+    ])
+})
+
 
 export default app
