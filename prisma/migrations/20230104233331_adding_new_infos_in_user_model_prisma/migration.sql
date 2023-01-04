a72953b8d02c4db22aa0ea8f66d4f1fb3687aa6b
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-04 20:40:10 -0300

    feat: adding new informations in user schema changes

---
+++ prisma/migrations/20230104233331_adding_new_infos_in_user_model_prisma/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "description" TEXT;
+ALTER TABLE "User" ADD COLUMN "githubUsername" TEXT;
+ALTER TABLE "User" ADD COLUMN "job" TEXT;
+ALTER TABLE "User" ADD COLUMN "skills" TEXT;
