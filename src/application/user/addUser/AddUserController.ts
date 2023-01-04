066a6096e88bbee5a32496555fa39cf5574c0c8f
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-04 20:40:38 -0300

    feat: add new infors in user

--- src/application/user/addUser/AddUserController.ts
+++ src/application/user/addUser/AddUserController.ts
@@ -4,14 +4,14 @@ import { Request, Response } from 'express'
 
 class AddUserController {
   async handle(request: Request, response: Response) {
-    let { admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo } : UserData = request.body
+    let { admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo, description, githubUsername, job, skills } : UserData = request.body
     let urlImage = request['file'].firebaseUrl
     if(urlImage){
       photo = urlImage
     }
 
     try {
-      const newUser = await factory.build({admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo})
+      const newUser = await factory.build({admin, confirmPassword, email, firstName, password, status, firstAcess, lastName, photo, description, githubUsername, job, skills})
       
       return response.status(201).json({ message: 'successfully create user!', data: newUser })
     } catch (error:any) {
