eb9f4ea8ecffda231386942a02178163e551bb96
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-04 20:40:57 -0300

    chore: hash password with a bcrypt

--- src/slices/user/useCases/addUser/AddUserUseCase.ts
+++ src/slices/user/useCases/addUser/AddUserUseCase.ts
@@ -1,5 +1,6 @@
 import { IAddUserRepository, IGetUserRepository } from '@/slices/user/repositories'
 import { UserData, UserEntity } from '../../entities/UserEntity'
+import { hash, genSalt } from 'bcrypt'
 
 
 export class AddUserUseCase {
@@ -12,6 +13,9 @@ export class AddUserUseCase {
             const userEntity = UserEntity.create(data)
             const existUser = await this.getUserRepository.getByEmail(data.email)            
             if(existUser) throw new Error('Email is already in use!')
+            const salt = await genSalt(10)
+            const newPassword = await hash(userEntity.password, salt)
+            userEntity.password = newPassword            
             Reflect.deleteProperty(userEntity, 'confirmPassword')
             const user = await this.addUserRepository.add(userEntity)            
             return user
