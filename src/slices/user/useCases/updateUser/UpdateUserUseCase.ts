61b292c412b6d99371ab9c78d8ed40df430676c8
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-06 20:57:06 -0300

    feat: create a updateUser useCase

---
+++ src/slices/user/useCases/updateUser/UpdateUserUseCase.ts
@@ -0,0 +1,18 @@
+import { IUpdateUserRepository, IGetUserRepository } from '../../repositories'
+
+export class UpdateUserUseCase {
+  constructor(
+    private updateUserRepository: IUpdateUserRepository,
+    private getUserRepository: IGetUserRepository
+  ) {}
+
+  async execute(id: string) {
+    try {
+      const user = await this.getUserRepository.getById(id)
+      if(user) throw new Error('not found user')
+      return this.updateUserRepository.update(id)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+}
