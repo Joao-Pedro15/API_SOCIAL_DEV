d34bd6ba00c1d8a10f1361b4f9c4828e90dc9dde
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-07 21:50:24 -0300

    chore: adding data param in updateUser useCase

--- src/slices/user/useCases/updateUser/UpdateUserUseCase.ts
+++ src/slices/user/useCases/updateUser/UpdateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { UserData } from '../../entities/UserEntity'
 import { IUpdateUserRepository, IGetUserRepository } from '../../repositories'
 
 export class UpdateUserUseCase {
@@ -6,11 +7,11 @@ export class UpdateUserUseCase {
     private getUserRepository: IGetUserRepository
   ) {}
 
-  async execute(id: string) {
+  async execute(id: string, data: Partial<UserData>) {
     try {
       const user = await this.getUserRepository.getById(id)
       if(!user) throw new Error('not found user')
-      return this.updateUserRepository.update(id)
+      return this.updateUserRepository.update(id, data)
     } catch (error) {
       throw new Error(error.message)
     }
