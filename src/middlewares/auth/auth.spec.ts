a6926a62910cb50313d1f0e81cd96355bf6c890c
João Pedro <jjoao.monteiro15@gmail.com>
2023-01-14 22:07:10 -0300

    test: testing invalid token not equal secret

--- src/middlewares/auth/auth.spec.ts
+++ src/middlewares/auth/auth.spec.ts
@@ -1,13 +1,21 @@
 import { authentication } from './auth'
 import { Request, Response, NextFunction } from 'express'
+const fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 describe('testing authenticate method', () => {
-  let request = { headers: { } } as Request
   let response = {} as Response
+  let request = { headers: { } } as Request
   const next = {} as NextFunction 
-
+  
   it('not existing autorization in headers request', async () => {
     const res = await authentication(request, response, next)
     expect(res).toEqual(false)
   })
+  it('invalid token failed authenticate', async() => {
+    request = { headers: { authorization: `Bearer ${fakeToken}` } } as Request
+    const res = await authentication(request, response, next)
+    expect(res).toBe('invalid signature')
+
+  })
+
 })
