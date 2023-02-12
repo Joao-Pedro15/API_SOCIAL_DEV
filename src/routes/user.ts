a4ace5bf7a357dd0b72fd3d7733ef7e053903045
João Pedro <jjoao.monteiro15@gmail.com>
2023-02-11 23:00:04 -0300

    feat: create a route from getUserById

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,12 +7,15 @@ import getUserController from '@/application/user/getUser/GetUserController'
 import loginUserController from '@/application/user/loginUser/LoginUserController'
 import deleteUserController from '@/application/user/deleteUser/DeleteUserController'
 import updateUserController from '@/application/user/updateUser/UpdateUserController'
+import getUserByIdController from '@/application/user/getUserById/GetUserByIdController'
 
 const router = Router()
 
 router.post('/addUser', Multer.single('file'), uploadImage, addUserController.handle)
 
 router.get('/getUsers', getUserController.handle)
+
+router.get('/getUsers/:id', getUserByIdController.handle)
 
 router.post('/loginUser', loginUserController.handle)
 
