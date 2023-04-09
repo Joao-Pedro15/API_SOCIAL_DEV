192d9e6399d735f567d92c2f13468efa64f343f2
João Pedro <joao.monteiro@veredaeducacao.com>
2023-04-09 17:55:28 -0300

    fix: solved bug in route getUser by id return json object

--- src/application/user/getUserById/GetUserByIdController.ts
+++ src/application/user/getUserById/GetUserByIdController.ts
@@ -4,8 +4,8 @@ import { Request, Response } from 'express'
 class GetUserByIdController {
   async handle(request: Request, response: Response) {
     try {
-      const user = factory.build(request.params.id)
-      return user
+      const user = await factory.build(request.params.id)
+      return response.status(200).json(user)
     } catch (error) {
       return response.status(500).json({message: error.message})
     }
