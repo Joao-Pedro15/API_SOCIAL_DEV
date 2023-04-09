65c59f894b0fc5e0bd6162a398e49b1f28c89fa5
João Pedro <joao.monteiro@veredaeducacao.com>
2023-04-09 17:54:56 -0300

    chore: delete migration

--- prisma/migrations/20230104233331_adding_new_infos_in_user_model_prisma/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "description" TEXT;
-ALTER TABLE "User" ADD COLUMN "githubUsername" TEXT;
-ALTER TABLE "User" ADD COLUMN "job" TEXT;
-ALTER TABLE "User" ADD COLUMN "skills" TEXT;
