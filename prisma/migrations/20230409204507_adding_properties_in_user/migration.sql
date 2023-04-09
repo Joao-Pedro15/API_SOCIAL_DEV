d4b78c591de980685076b4bd77482b9d88e64ce2
João Pedro <joao.monteiro@veredaeducacao.com>
2023-04-09 17:58:14 -0300

    feat: create new migration

---
+++ prisma/migrations/20230409204507_adding_properties_in_user/migration.sql
@@ -0,0 +1,22 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[User] ADD [description] NVARCHAR(1000),
+[githubUsername] NVARCHAR(1000),
+[job] NVARCHAR(1000),
+[skills] NVARCHAR(1000);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
